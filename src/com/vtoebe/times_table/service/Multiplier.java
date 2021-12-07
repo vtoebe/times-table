73435c4df2495c61394c641a01b0731e117f47a9
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-07 02:28:59 -0300

    Update Multiplier.java

--- src/com/vtoebe/times_table/service/Multiplier.java
+++ src/com/vtoebe/times_table/service/Multiplier.java
@@ -1,4 +1,12 @@
 package com.vtoebe.times_table.service;
 
 public class Multiplier {
+    int[] timesTable = new int[9];
+
+    public int[] multiply(int number){
+        for (int i = 0; i < 10; i += 1){
+            timesTable[i] = number * i;
+        }
+        return timesTable;
+    }
 }

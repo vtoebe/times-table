7aa2569faf0e7f522798d34ad3829fa3d097b914
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-07 04:04:59 -0300

    feat: add result printer

--- src/com/vtoebe/times_table/utils/Printer.java
+++ src/com/vtoebe/times_table/utils/Printer.java
@@ -3,7 +3,14 @@ package com.vtoebe.times_table.utils;
 import javax.swing.*;
 
 public class Printer {
+    final String REQUIRE_NUMBER = "Write a number to get it's time table!";
     final String CONTINUE_LINE = "Would you like to continue?";
 
+    public String inputNumber(){ return JOptionPane.showInputDialog(null, REQUIRE_NUMBER, "Times Table Generator", JOptionPane.INFORMATION_MESSAGE); }
+
     public int continueCheck(){ return JOptionPane.showConfirmDialog(null, CONTINUE_LINE, "Continue", JOptionPane.YES_NO_OPTION); }
+
+    public void timesTableResult(String[] multiplied) { JOptionPane.showMessageDialog(null, new JList<>(multiplied)); }
+
+
 }

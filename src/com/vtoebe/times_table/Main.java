d06fa508df2b4846d02c07239741501751957128
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-07 03:59:36 -0300

    Update Main

--- src/com/vtoebe/times_table/Main.java
+++ src/com/vtoebe/times_table/Main.java
@@ -1,7 +1,28 @@
 package com.vtoebe.times_table;
 
+import com.vtoebe.times_table.service.ContinuityChecker;
+import com.vtoebe.times_table.service.Multiplier;
+import com.vtoebe.times_table.utils.Printer;
+import com.vtoebe.times_table.utils.Writer;
+
 public class Main {
-    private static void main(String[] args){
-        
+    public static void main(String[] args){
+
+        Printer print = new Printer();
+        Writer write = new Writer();
+        Multiplier mult = new Multiplier();
+        ContinuityChecker continuity = new ContinuityChecker();
+
+        do {
+            print.timesTableResult(
+                    write.generatedTable(
+                            mult.multiply(
+                                    write.saveInputNumber(
+                                            print.inputNumber()
+                                    )
+                            )
+                    )
+            );
+        } while( continuity.check() ) ;
     }
 }

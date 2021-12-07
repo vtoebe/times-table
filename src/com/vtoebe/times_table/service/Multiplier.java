a90a6b4fde60b640a47c57868f0376fe2172e6ff
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-07 04:05:59 -0300

    Fix: fix for loop logic

--- src/com/vtoebe/times_table/service/Multiplier.java
+++ src/com/vtoebe/times_table/service/Multiplier.java
@@ -1,11 +1,11 @@
 package com.vtoebe.times_table.service;
 
 public class Multiplier {
-    int[] timesTable = new int[9];
+    int[] timesTable = new int[10];
 
     public int[] multiply(int number){
-        for (int i = 0; i < 10; i += 1){
-            timesTable[i] = number * i;
+        for (int i = 1; i <= 10; i += 1){
+            timesTable[i-1] = number * i;
         }
         return timesTable;
     }
